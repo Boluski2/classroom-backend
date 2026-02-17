fe1cbbdf6f815dda50958065a7f1851094fd4da9
Boluski1 <babatundebolu@rocketmail.com>
2026-02-17 11:05:03 +0100

    Get all subject Route

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,23 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
+import subjectRouter from "./routes/subjects";
 
 const app = express();
 const PORT = 8000;
 
 // JSON middleware
 app.use(express.json());
+
+app.use('/api/subjects', subjectRouter);
+
+app.use(cors ({
+ origin: process.env.FRONTEND_URL,
+ methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//  allowedHeaders: ['Content-Type', 'Authorization'],
+ credentials: true,
+}))
+
+
 
 // Root route
 app.get("/", (req: Request, res: Response) => {
